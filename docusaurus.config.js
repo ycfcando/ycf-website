a0e16242578de7d006bc8667376aba9aa7febae4
yechaofan <yechaofan@bytedance.com>
2022-10-21 18:46:13 +0800

    feat: 首页&配置

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,23 +6,20 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Hypoxia',
-  tagline: '任何迷茫都源自不自律',
+  title: 'Real Own Things',
+  baseUrl: '/',
   url: 'https://ycfcando.github.io',
-  baseUrl: '/my-website/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+
+  tagline: 'Write down my thoughts',
   favicon: 'img/favicon.jpg',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ycfcando', // Usually your GitHub org/user name.
-  projectName: 'my-website', // Usually your repo name.
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  organizationName: 'ycfcando',
+  projectName: 'my-website',
   trailingSlash: false,
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'zh-Hans',
     locales: ['zh-Hans'],
@@ -34,18 +31,10 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+
         },
         blog: {
           showReadingTime: false,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -58,28 +47,35 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       colorMode: {
-        disableSwitch: true,
+        disableSwitch: false,
       },
       navbar: {
         title: 'Hypoxia',
         logo: {
           alt: 'logo',
-          src: 'img/favicon.jpg',
+          src: '/img/favicon.jpg',
+          style: {
+            borderRadius: '50%'
+          }
         },
         items: [
           {
-            type: 'doc',
-            docId: 'intro',
-            position: 'right',
-            label: '软件',
+            type: 'dropdown',
+            position: 'left',
+            label: 'Software',
+            items: []
           },
           {
-            type: 'doc',
-            docId: 'tutorial-basics/congratulations',
-            position: 'right',
-            label: '硬件',
+            type: 'dropdown',
+            position: 'left',
+            label: 'Hardware',
+            items: []
           },
-          {to: '/blog', label: '杂记', position: 'right'}
+          {
+            to: '/blog',
+            label: 'Blog',
+            position: 'left'
+          }
         ],
       },
       prism: {
